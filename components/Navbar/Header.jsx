2ec78f97a02c42c209bde9eaba7aa11582acf71c
whitepink1 <kiruhat9@gmail.com>
2025-04-29 15:59:48 +0300

    Created Header Promo Bar

---
+++ components/Navbar/Header.jsx
@@ -0,0 +1,10 @@
+import HeaderPromotion from "./HeaderPromotion";
+
+const Header = () => {
+    return(<div className="flex-row-center">
+        <HeaderPromotion />
+        Header
+    </div>)
+}
+
+export default Header;
