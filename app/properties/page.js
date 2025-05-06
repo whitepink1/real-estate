5bd140da940f8b496dd27d8fe9452e782b297c08
whitepink1 <kiruhat9@gmail.com>
2025-05-06 22:09:54 +0300

    Finished Properties Page

--- app/properties/page.js
+++ app/properties/page.js
@@ -1,4 +1,10 @@
 import FindProperty from "@/components/FindProperty";
+import PropertiesForm from "@/components/PropertiesForm";
+import ComponentHeader from "@/components/Shared/ComponentHeader";
+import PromotingText from "@/components/Shared/PromotingText";
+import PropertiesSliderCard from "@/components/Shared/PropertiesSliderCard";
+import Slidercard from "@/components/Shared/Slidercard";
+import { PropertiesData } from "@/constants/constant";
 
 export const metadata = {
     title: "About Us",
@@ -7,5 +13,22 @@ export const metadata = {
 export default function Page() {
     return (<div className="w-full">
         <FindProperty />
+        <ComponentHeader 
+                name='Discover a World of Possibilities'
+                description="Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home"
+                typeOfCard="half"
+                addClass=""/>
+        <Slidercard borders={false} buttonName="View All Properties">
+            {PropertiesData.map((item, id) => (
+            <PropertiesSliderCard key={id} property={item} />
+            ))}
+        </Slidercard>
+        <ComponentHeader 
+                name="Let's Make it Happen"
+                description="Ready to take the first step toward your dream property? Fill out the form below, and our real estate wizards will work their magic to find your perfect match. Don't wait; let's embark on this exciting journey together."
+                typeOfCard="half"
+                addClass=""/>
+        <PropertiesForm />
+        <PromotingText />
     </div>)
 }
---
+++ components/Shared/SelectOption.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+const SelectOption = ({img = "", name, options}) => {
+    return(<div className={`flex items-center relative w-full 
+                ${img ? 'px-4 h-[52px] bg-grey-08 border-2 border-grey-15 rounded-xl' 
+                : 'py-4 px-5 rounded-lg bg-grey-10 border-1 border-grey-15'}`}>
+        {img && <Image className="mr-2 border-r-2 pr-1 border-grey-15 aspect-square" src={img} height={25} width={25} alt={name} />}
+        <select className={`${img ? 'w-full h-full font-paragraph font-light text-grey-60 appearance-none pr-6 focus:outline-none' 
+                : 'w-full pr-6 font-paragraph text-grey-60 bg-grey-10 appearance-none focus:outline-none'}`}
+                defaultValue="">
+            <option value="" disabled hidden>
+                {name}
+            </option>
+            {options.map((item) => (
+                <option key={item} value={item}>{item}</option>
+            ))}
+        </select>
+        <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2">
+            <svg
+            className={`w-4 h-4 ${img ? 'text-grey-60' : 'text-white'}`}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            >
+            <path d="M19 9l-7 7-7-7" />
+            </svg>
+      </div>
+    </div>)
+}
+
+export default SelectOption;
